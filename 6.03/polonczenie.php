ec69651d2e362512ee58dbc73482696ed93eed88
Bartek <247160529+bartus2108-cell@users.noreply.github.com>
2026-03-13 08:29:47 +0100

    ...

--- 6.03/polonczenie.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "127.0.0.1";
-$dbname = "sklep";
-$username = "root";
-$password = "";
-
-$db = mysqli_connect($host, $username, $password, $dbname);
-if(!$db) error_log("Wystąpił Błąd: ". mysqli_connect_error());
-?>
